98915c1a19628ebed25659c8996a47418a5b74d6
vitalis-virtus <68068673+vitalis-virtus@users.noreply.github.com>
2022-05-14 03:27:55 +0300

    Updates

--- src.a38bb705.js
+++ src.a38bb705.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/nios_courses_design1/src.f8e29cce.js.map
+//# sourceMappingURL=/nios_courses_design1/src.a38bb705.js.map
